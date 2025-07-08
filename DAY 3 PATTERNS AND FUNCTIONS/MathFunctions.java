8c7ad0e4b679204803854d616591f9b5eba1c9d0
shraddha241104 <shraddha241104@gmail.com>
2025-07-08 13:23:52 +0530

    functions completed

---
+++ DAY 3 PATTERNS AND FUNCTIONS/MathFunctions.java
@@ -0,0 +1,9 @@
+public class MathFunctions {
+    public static void main(String args[]){
+        System.out.println("Maximum of numbers: "+ Math.max(6, 5));
+        System.out.println("Minimum of numbers: "+ Math.min(6, 5));
+        System.out.println("Squareroot of number: "+ Math.sqrt(8));
+        System.out.println("Power: "+ Math.pow(2, 3));
+        System.out.println("Absolute of number: "+ Math.abs(-8));//gives positive of value entered
+    }
+}
