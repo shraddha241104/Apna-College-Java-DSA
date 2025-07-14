1994a1a98f36b4deeea48e468144ba17bf3cc5ca
shraddha241104 <shraddha241104@gmail.com>
2025-07-14 12:38:12 +0530

    started with arrays

---
+++ DAY 4 ARRAYS/ArrayBasics.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class ArrayBasics {
+
+    public static void main(String args[]) {
+        int marks[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = sc.nextInt();
+        }
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 3;
+        }
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+        System.out.println(marks.length);
+    }
+}
