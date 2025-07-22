6f66a571dd7983e59cd2482b38a550b53461909b
shraddha241104 <shraddha241104@gmail.com>
2025-07-22 23:29:29 +0530

    Selection sort descending order - Assignment 2 done

---
+++ ASSIGNMENTS/DAY 5 ASSIGNMENT/SelectionSortDesc.java
@@ -0,0 +1,31 @@
+
+public class SelectionSortDesc {
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] < arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+        selectionSort(arr);
+        printArray(arr);
+
+    }
+}
