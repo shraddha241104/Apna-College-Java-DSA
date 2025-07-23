313639862e2c618484f1334961cf281f2dc58409
shraddha241104 <shraddha241104@gmail.com>
2025-07-24 00:04:11 +0530

    diagonal sum with optimization done

--- DAY 6 2D ARRAYS/DiagonalSum.java
+++ DAY 6 2D ARRAYS/DiagonalSum.java
@@ -5,11 +5,26 @@ public class DiagonalSum {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
+                //pd
                 if (i == j) {
                     sum += arr[i][j];
-                } else if (i + j == arr.length - 1) {
+                } //sd
+                else if (i + j == arr.length - 1) {
                     sum += arr[i][j];
                 }
+            }
+        }
+        return sum;
+    }
+
+    public static int diagonalSumOptimized(int arr[][]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            //pd
+            sum += arr[i][i];
+            //sd
+            if (i != arr.length - 1 - i) {
+                sum += arr[i][arr.length - 1 - i];
             }
         }
         return sum;
@@ -19,5 +34,7 @@ public class DiagonalSum {
         int matrix[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         int sum = diagonalSum(matrix);
         System.out.println("primary diagonal sum + secondary diagonal sum = " + sum);
+        int sum2 = diagonalSumOptimized(matrix);
+        System.out.println("primary diagonal sum + secondary diagonal sum = " + sum2);
     }
 }
