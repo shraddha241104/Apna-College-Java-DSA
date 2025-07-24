f85b82d5f61a1d966d278ebc0cfec8fee5dcebb2
shraddha241104 <shraddha241104@gmail.com>
2025-07-24 14:09:06 +0530

    Palindrome string done

---
+++ DAY 7 STRINGS/PalindromeString.java
@@ -0,0 +1,22 @@
+
+public class PalindromeString {
+
+    public static boolean checkPalindrome(String str) {
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        String str = "racecar";
+        if (checkPalindrome(str)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+}
