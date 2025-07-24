f24389d3cfdc4ead3b1332eeb54d5f2e922209c9
shraddha241104 <shraddha241104@gmail.com>
2025-07-24 14:09:57 +0530

    shortest path done

---
+++ DAY 7 STRINGS/ShortestPath.java
@@ -0,0 +1,29 @@
+
+public class ShortestPath {
+
+    public static int findShortestPath(String str) {
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 0;
+        int y2 = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'N') {
+                y2++;
+            } else if (str.charAt(i) == 'E') {
+                x2++;
+            } else if (str.charAt(i) == 'W') {
+                x2--;
+            } else {
+                y2--;
+            }
+        }
+        int shortestPath = (int) Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+        return shortestPath;
+    }
+
+    public static void main(String args[]) {
+        String directions = "WNEENESENNN";
+        int shortestPath = findShortestPath(directions);
+        System.out.println("shortest path = " + shortestPath);
+    }
+}
