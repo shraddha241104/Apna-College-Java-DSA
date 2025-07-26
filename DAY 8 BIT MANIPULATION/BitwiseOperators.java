4a2a41fb2f99f58b94b89daf7d8da4bf65b1a88a
shraddha241104 <shraddha241104@gmail.com>
2025-07-26 23:42:16 +0530

    Basics Bitwise operators done

---
+++ DAY 8 BIT MANIPULATION/BitwiseOperators.java
@@ -0,0 +1,12 @@
+
+public class BitwiseOperators {
+
+    public static void main(String args[]) {
+        System.out.println(5 & 6); // Binary AND
+        System.out.println(5 | 6); //Binary OR
+        System.out.println(5 ^ 6); //Binary XOR
+        System.out.println(~5); //Binary one's complement
+        System.out.println(5 << 2); //Binary Left Shift
+        System.out.println(5 >> 2); //Binary Right Shift
+    }
+}
