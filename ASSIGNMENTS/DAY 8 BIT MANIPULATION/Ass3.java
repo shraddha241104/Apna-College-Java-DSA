0cdad9948f73439e0814da17a081d8720c304a24
shraddha241104 <shraddha241104@gmail.com>
2025-07-28 00:11:49 +0530

    add 1 to integer - Assignment 3 Bit Manipulation done

---
+++ ASSIGNMENTS/DAY 8 BIT MANIPULATION/Ass3.java
@@ -0,0 +1,12 @@
+
+public class Ass3 {
+
+    public static void main(String[] args) {
+        int a = 6;
+        System.out.println(a + " + " + 1 + " is " + -~a);
+        a = -4;
+        System.out.println(a + " + " + 1 + " is " + -~a);
+        a = 0;
+        System.out.println(a + " + " + 1 + " is " + -~a);
+    }
+}
