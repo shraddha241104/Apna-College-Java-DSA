3ad324d41cc5e0a41eaef5733fcb3ba1e4796649
shraddha241104 <shraddha241104@gmail.com>
2025-07-28 00:12:58 +0530

    converted uppercase to lowercase - assignment 4 bit manipulation done

---
+++ ASSIGNMENTS/DAY 8 BIT MANIPULATION/Ass4.java
@@ -0,0 +1,9 @@
+
+public class Ass4 {
+
+    public static void main(String args[]) {
+        for (int ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print((char) (ch | ' '));
+        }
+    }
+}
