bbc4865a9f4c26495a44e11c172cf0a91ba86168
shraddha241104 <shraddha241104@gmail.com>
2025-07-29 21:49:54 +0530

    Classes and objects understood and implemented

---
+++ DAY 9 OBJECT ORIENTED PROGRAMMING/ClassesAndObjects.java
@@ -0,0 +1,25 @@
+
+public class ClassesAndObjects {
+
+    public static void main(String args[]) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+    }
+}
+
+class Pen {
+
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
