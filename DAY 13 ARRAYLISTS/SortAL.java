2c60269cfc93e68e9a7bfab6dedd08924b98401d
shraddha241104 <shraddha241104@gmail.com>
2025-11-16 21:13:25 +0530

    Sorting an ArrayList done

---
+++ DAY 13 ARRAYLISTS/SortAL.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class SortAL {
+
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        System.out.println("Before Sorting");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println("After Sorting");
+        System.out.println(list);
+    }
+}
