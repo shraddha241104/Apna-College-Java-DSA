6aca499ae4ad204080b9a7d0205681f96a005c89
shraddha241104 <shraddha241104@gmail.com>
2025-11-18 23:38:48 +0530

    optimized maximum water container

---
+++ DAY 13 ARRAYLISTS/OptimizedContainerWater.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class OptimizedContainerWater {
+
+    public static int storeWater(ArrayList<Integer> heights) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = heights.size() - 1;
+        while (lp < rp) {
+            int ht = Math.min(heights.get(lp), heights.get(rp));
+            int width = rp - lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+            if (lp < rp) {
+                lp++;
+            } else {
+                rp++;
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> heights = new ArrayList<>();
+        heights.add(1);
+        heights.add(8);
+        heights.add(6);
+        heights.add(2);
+        heights.add(5);
+        heights.add(4);
+        heights.add(8);
+        heights.add(2);
+        heights.add(7);
+        System.out.println("Maximum water stored by the container is : " + storeWater(heights));
+    }
+}
