bfa840dae28084801a8a185a7742e8859099e1a8
shraddha241104 <shraddha241104@gmail.com>
2025-11-20 10:33:41 +0530

    optimized pair sum 2 (sorted and rotated array)

--- DAY 13 ARRAYLISTS/OptimizedPairSum2.java
+++ DAY 13 ARRAYLISTS/OptimizedPairSum2.java
@@ -1,22 +1,27 @@
 
 import java.util.*;
 
-public class OptimizedPairSum1 {
+public class OptimizedPairSum2 {
 
     public static boolean pairSum(ArrayList<Integer> list, int target) {
         int lp = 0;
-        int rp = list.size() - 1;
+        int rp = 0;
+        int n = list.size();
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                rp = i;
+                lp = i + 1;
+                break;
+            }
+        }
         while (lp != rp) {
-            //case1
             if (list.get(lp) + list.get(rp) == target) {
                 return true;
             }
-            //case2
             if (list.get(lp) + list.get(rp) > target) {
-                rp--;
-            } //case3
-            else {
-                lp++;
+                rp = (n + rp - 1) % n;
+            } else {
+                lp = (lp + 1) % n;
             }
         }
         return false;
@@ -24,13 +29,13 @@ public class OptimizedPairSum1 {
 
     public static void main(String args[]) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        list.add(11);
+        list.add(15);
         list.add(6);
-        int target = 5;
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16;
         System.out.println(pairSum(list, target));
     }
 }
