6a4a50cdf4700e6748bb86976e5149e1247fdf59
shraddha241104 <shraddha241104@gmail.com>
2025-11-22 10:59:09 +0530

    Palindrome linked list

---
+++ DAY 14 LINKED LISTS/PalindromeLinkedList.java
@@ -0,0 +1,122 @@
+
+public class PalindromeLinkedList {
+
+    public static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    //slow fast approach
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    //check palindrome
+    public boolean checkPalindrome() {
+        //base case
+        if (head == null && head.next == null) {
+            return true;
+        }
+        //step 1 : find mid
+        Node mid = findMid(head);
+        //step 2 : reverse the 2nd half
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        //step 3 : check left half and right half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    public void printLL() {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+        PalindromeLinkedList ll = new PalindromeLinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.printLL();
+        Boolean result = ll.checkPalindrome();
+        System.out.println("Result of Palindrome or not: " + result);
+    }
+}
