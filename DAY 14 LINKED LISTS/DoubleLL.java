84816af053b8366c6cc336e426c62a40105ca66f
shraddha241104 <shraddha241104@gmail.com>
2025-11-26 12:44:34 +0530

    Double linked list add remove reverse

---
+++ DAY 14 LINKED LISTS/DoubleLL.java
@@ -0,0 +1,91 @@
+
+public class DoubleLL {
+
+    public static class Node {
+
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            size++;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+        size++;
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int value = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return value;
+    }
+
+    public void printLL() {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public static void main(String args[]) {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.printLL();
+        System.out.println("size : " + dll.size);
+        dll.removeFirst();
+        dll.printLL();
+        System.out.println("size : " + dll.size);
+        dll.reverse();
+        dll.printLL();
+    }
+}
