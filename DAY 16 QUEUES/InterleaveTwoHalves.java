1cfbb931f5861d4647429bfba844e887b10b3dca
shraddha241104 <shraddha241104@gmail.com>
2025-12-03 00:57:16 +0530

    interleave two halves of a queue

---
+++ DAY 16 QUEUES/InterleaveTwoHalves.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class InterleaveTwoHalves {
+
+    public static void interleaving(Queue<Integer> q) {
+        int n = q.size();
+        Queue<Integer> q1 = new LinkedList<>();
+        for (int i = 0; i < n / 2; i++) {
+            q1.add(q.remove());
+        }
+        while (!q1.isEmpty()) {
+            q.add(q1.remove());
+            q.add(q.remove());
+        }
+    }
+
+    public static void main(String args[]) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        interleaving(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
