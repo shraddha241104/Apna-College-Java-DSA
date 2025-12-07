21a437a2f6f9248051a625ed70abc2d5c2a51a7f
shraddha241104 <shraddha241104@gmail.com>
2025-12-07 17:14:40 +0530

    Root to leaf BST

---
+++ DAY 19 BINARY SEARCH TREE/RootToLeaf.java
@@ -0,0 +1,65 @@
+
+import java.util.*;
+
+public class RootToLeaf {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void rootToLeaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.data);
+        if (root.left == null && root.right == null) {
+            System.out.println(path);
+        }
+        rootToLeaf(root.left, path);
+        rootToLeaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        ArrayList<Integer> path = new ArrayList<>();
+        rootToLeaf(root, path);
+    }
+}
