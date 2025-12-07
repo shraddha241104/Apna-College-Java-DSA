73eec0954bc0032550e109bde2fb412230699db2
shraddha241104 <shraddha241104@gmail.com>
2025-12-08 00:26:39 +0530

    BST to Balanced BST

---
+++ DAY 19 BINARY SEARCH TREE/BSTToBalancedBST.java
@@ -0,0 +1,68 @@
+
+import java.util.*;
+
+public class BSTToBalancedBST {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void getInorder(Node root, ArrayList<Integer> inorder) {
+        if (root == null) {
+            return;
+        }
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+
+    public static Node createBST(ArrayList<Integer> inorder, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBST(inorder, start, mid - 1);
+        root.right = createBST(inorder, mid + 1, end);
+        return root;
+    }
+
+    public static Node balancedBST(Node root) {
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+        root = createBST(inorder, 0, inorder.size() - 1);
+        return root;
+    }
+
+    public static void inorderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
+    }
+
+    public static void main(String args[]) {
+        Node root = new Node(8);
+        root.left = new Node(6);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(3);
+
+        root.right = new Node(10);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
+
+        root = balancedBST(root);
+        inorderTraversal(root);
+    }
+}
