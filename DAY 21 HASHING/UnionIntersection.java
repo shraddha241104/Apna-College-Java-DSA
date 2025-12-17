15f73903cd713c473a3ced06a97d0dfb78a52268
shraddha241104 <shraddha241104@gmail.com>
2025-12-17 23:31:33 +0530

    Union and Intersection

---
+++ DAY 21 HASHING/UnionIntersection.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class UnionIntersection {
+
+    public static void main(String args[]) {
+        int arr1[] = {7, 3, 9};
+        int arr2[] = {6, 3, 9, 2, 9, 4};
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+        System.out.println("union = " + set.size());
+        System.out.println(set);
+        set.clear();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            if (set.contains(arr2[i])) {
+                count++;
+                System.out.print(arr2[i] + " ");
+                set.remove(arr2[i]);
+            }
+        }
+        System.out.println();
+        System.out.println("Intersection = " + count);
+    }
+}
