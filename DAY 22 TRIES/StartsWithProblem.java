faf02fa3b05c611c3e7cf0711250a0fc1dd661e9
shraddha241104 <shraddha241104@gmail.com>
2025-12-19 16:48:09 +0530

    Starts With Problem

---
+++ DAY 22 TRIES/StartsWithProblem.java
@@ -0,0 +1,63 @@
+
+public class StartsWithProblem {
+
+    static class Node {
+
+        Node children[] = new Node[26];
+        boolean eow;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    public static boolean startsWith(String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        String arr[] = {"apple", "app", "mango", "man", "woman"};
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+        String prefix1 = "app";
+        String prefix2 = "moon";
+        System.out.println(startsWith(prefix1));
+        System.out.println(startsWith(prefix2));
+    }
+}
