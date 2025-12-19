bbaf77ecb27aacd91cc0ea4b5a19d3476c473205
shraddha241104 <shraddha241104@gmail.com>
2025-12-19 17:50:32 +0530

    Longest Word with all prefixes

---
+++ DAY 22 TRIES/LongestWordPrefixesProblem.java
@@ -0,0 +1,67 @@
+
+public class LongestWordPrefixesProblem {
+
+    static class Node {
+
+        Node children[] = new Node[26];
+        boolean eow;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            int idx = key.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+    public static String ans = "";
+
+    public static void longestWord(Node root, StringBuilder temp) {
+        if (root == null) {
+            return;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null && root.children[i].eow == true) {
+                char ch = (char) (i + 'a');
+                temp.append(ch);
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        String arr[] = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+        longestWord(root, new StringBuilder());
+        System.out.println(ans);
+    }
+}
