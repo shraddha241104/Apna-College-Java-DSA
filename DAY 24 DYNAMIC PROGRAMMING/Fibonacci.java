5b97df84f182ef2531033021849a3b6cdb02c8b4
shraddha241104 <shraddha241104@gmail.com>
2025-12-25 14:04:36 +0530

    Fibonacci - Memoization & Tabulation

--- DAY 24 DYNAMIC PROGRAMMING/Fibonacci.java
+++ DAY 24 DYNAMIC PROGRAMMING/Fibonacci.java
@@ -12,10 +12,21 @@ public class Fibonacci {
         return fib[n];
     }
 
+    public static int fibTabulation(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String args[]) {
         int n = 5;
         int fib[] = new int[n + 1];
         int ans = getFib(n, fib);
         System.out.println(ans);
+        System.out.println(fibTabulation(n));
     }
 }
