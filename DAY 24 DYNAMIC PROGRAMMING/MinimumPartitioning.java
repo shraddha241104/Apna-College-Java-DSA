414be99a895ff8e73f96e1f4a632a58728bd98e7
shraddha241104 <shraddha241104@gmail.com>
2025-12-29 02:35:13 +0530

    Minimum Partitioning

---
+++ DAY 24 DYNAMIC PROGRAMMING/MinimumPartitioning.java
@@ -0,0 +1,37 @@
+
+public class MinimumPartitioning {
+
+    //O(n*W) - Knapsack 0-1 variation
+    public static int minPartition(int arr[]) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        int W = sum / 2;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] = Math.max(arr[i - 1] + dp[i - 1][j - arr[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1 - sum2);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 6, 11, 5};
+        System.out.println(minPartition(arr));
+    }
+}
