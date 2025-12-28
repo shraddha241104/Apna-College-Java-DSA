69615f5015896e7fe4331e80f020e19f8cbfdc78
shraddha241104 <shraddha241104@gmail.com>
2025-12-28 22:54:16 +0530

    Count BSTs

---
+++ DAY 24 DYNAMIC PROGRAMMING/CountBSTs.java
@@ -0,0 +1,23 @@
+
+public class CountBSTs {
+
+    //O(n*n)
+    public static int countTrees(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                int left = dp[j];
+                int right = dp[i - j - 1];
+                dp[i] += left * right;
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String args[]) {
+        int n = 4;
+        System.out.println(countTrees(n));
+    }
+}
