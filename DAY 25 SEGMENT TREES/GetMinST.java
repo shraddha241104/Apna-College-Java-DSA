c0e976ce40c45862f2f4f6ae3800cd650d73bb63
shraddha241104 <shraddha241104@gmail.com>
2025-12-30 02:31:11 +0530

    Get Minimum from Segment Tree

--- DAY 25 SEGMENT TREES/GetMinST.java
+++ DAY 25 SEGMENT TREES/GetMinST.java
@@ -1,5 +1,5 @@
 
-public class GetMaxST {
+public class GetMinST {
 
     static int tree[];
 
@@ -16,26 +16,26 @@ public class GetMaxST {
         int mid = (si + sj) / 2;
         buildTree(2 * i + 1, si, mid, arr);
         buildTree(2 * i + 2, mid + 1, sj, arr);
-        tree[i] = Math.max(tree[2 * i + 1], tree[2 * i + 2]);
+        tree[i] = Math.min(tree[2 * i + 1], tree[2 * i + 2]);
     }
 
-    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
+    public static int getMinUtil(int i, int si, int sj, int qi, int qj) {
         if (si > qj || sj < qi) {
-            return Integer.MIN_VALUE;
+            return Integer.MAX_VALUE;
         } else if (si >= qi && sj <= qj) {
             return tree[i];
         } else {
             int mid = (si + sj) / 2;
-            int leftAns = getMaxUtil(2 * i + 1, si, mid, qi, qj);
-            int rightAns = getMaxUtil(2 * i + 2, mid + 1, sj, qi, qj);
-            return Math.max(leftAns, rightAns);
+            int leftAns = getMinUtil(2 * i + 1, si, mid, qi, qj);
+            int rightAns = getMinUtil(2 * i + 2, mid + 1, sj, qi, qj);
+            return Math.min(leftAns, rightAns);
         }
     }
 
     //O(log n)
-    public static int getMax(int arr[], int qi, int qj) {
+    public static int getMin(int arr[], int qi, int qj) {
         int n = arr.length;
-        return getMaxUtil(0, 0, n - 1, qi, qj);
+        return getMinUtil(0, 0, n - 1, qi, qj);
     }
 
     public static void updateUtil(int i, int si, int sj, int idx, int newVal) {
@@ -45,8 +45,9 @@ public class GetMaxST {
         if (si == sj) {
             tree[i] = newVal;
         }
+
         if (si != sj) {
-            tree[i] = Math.max(tree[i], newVal);
+            tree[i] = Math.min(tree[i], newVal);
             int mid = (si + sj) / 2;
             updateUtil(2 * i + 1, si, mid, idx, newVal);
             updateUtil(2 * i + 2, mid + 1, sj, idx, newVal);
@@ -69,10 +70,10 @@ public class GetMaxST {
             System.out.print(tree[i] + " ");
         }
         System.out.println();
-        int max = getMax(arr, 2, 5);
+        int max = getMin(arr, 2, 5);
         System.out.println(max);
         update(arr, 2, 20);
-        int maxNew = getMax(arr, 2, 5);
+        int maxNew = getMin(arr, 2, 5);
         System.out.println(maxNew);
     }
 }
