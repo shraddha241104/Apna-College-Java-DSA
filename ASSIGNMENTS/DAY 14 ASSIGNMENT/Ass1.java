75c9de12e41a0b82d5a5653186e22471421b00b0
shraddha241104 <shraddha241104@gmail.com>
2026-01-02 23:23:41 +0530

    Assignment 1 - intersection node

---
+++ ASSIGNMENTS/DAY 14 ASSIGNMENT/Ass1.java
@@ -0,0 +1,56 @@
+
+public class Ass1 {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node intersection(Node head1, Node head2) {
+        while (head2 != null) {
+            Node temp = head1;
+            while (temp != null) {
+                if (temp == head2) {
+                    return head2;
+                }
+                temp = temp.next;
+            }
+            head2 = head2.next;
+        }
+        return null;
+    }
+
+    public static void main(String args[]) {
+        Ass1 list = new Ass1();
+        Node head1, head2;
+        head1 = new Node(10);
+        head2 = new Node(3);
+        Node newNode = new Node(6);
+        head2.next = newNode;
+
+        newNode = new Node(9);
+        head2.next.next = newNode;
+
+        newNode = new Node(15);
+        head1.next = newNode;
+        head2.next.next.next = newNode;
+
+        newNode = new Node(30);
+        head1.next.next = newNode;
+        head1.next.next.next = null;
+
+        Node intersectionPoint = list.intersection(head1, head2);
+
+        if (intersectionPoint == null) {
+            System.out.print("No Intersection Point\n");
+        } else {
+            System.out.print("Intersection Point: " + intersectionPoint.data);
+        }
+    }
+}
